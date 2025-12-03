52a77b57abe90eac9f54e641dc30c1f10bcdc376
Stanislav Selunin <infleshlightwetrust@gmail.com>
2025-12-03 12:51:42 +0300

    Implement LizardWizards second ability

--- GameVue/src/gameObjects/Characters/LizardWizard.ts
+++ GameVue/src/gameObjects/Characters/LizardWizard.ts
@@ -1,6 +1,5 @@
 import { PlayerController } from '../../../managers/PlayerController';
 import { GameScene } from '../../scenes/Game';
-import ASSETS from '../../assets';
 
 export class LizardWizard extends PlayerController {
     constructor(scene: GameScene, x: number, y: number) {
@@ -26,13 +25,35 @@ export class LizardWizard extends PlayerController {
         this.startAbility1Cooldown();
     }
 
-    protected ability2(): void {
+    // Higher the spread value the tighter the spread
+    protected ability2(spread = 6, amountOfProjectiles = 3): void {
         if (!this.canUseAbility2()) return;
+        const yDifference = this.currentAim.y - this.y;
+        const xDifference = this.currentAim.x - this.x;
+        const distance = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+        const rotation = Math.atan2(yDifference, xDifference);
 
-        // Wizard special: spread shot (stub for now)
-        console.log('LizardWizard spread shot!');
-        // TODO: Implement spread shot later
-        // Could fire 3 projectiles in a spread pattern
+        const totalSpreadAngle = Math.PI / spread;
+        const anglePerProjectile = totalSpreadAngle / (amountOfProjectiles - 1);
+        const startAngle = rotation - (totalSpreadAngle / 2);
+
+        const projectileTrajectories = [];
+        for(let i = 0; i < amountOfProjectiles; i++) {
+            const currentAngle = startAngle + (i * anglePerProjectile);
+            const xLeftTo = this.x + (distance * Math.cos(currentAngle));
+            const yLeftTo = this.y + (distance * Math.sin(currentAngle));
+            projectileTrajectories.push({
+                x: xLeftTo,
+                y: yLeftTo
+            })
+        }
+
+        projectileTrajectories.forEach((trajectory) => {
+            this.gameScene.fireBullet(
+                {x: this.x, y: this.y},
+                {x: trajectory.x, y: trajectory.y}
+            );
+        })
 
         this.startAbility2Cooldown();
     }
